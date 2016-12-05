363383177a709739ba69d53cf8664cc98261d080
michaelsampietro <mssampietro@gmail.com>
2016-12-05 14:05:30 -0200

    arquivos novos

--- handler.php
+++ handler.php
@@ -6,6 +6,7 @@
     ?>
 <!-- Estrutura básica da biblioteca de Chart JS para criar um gráfico -->
 <div class="container">
+    <div class="row">
         <div class="grafico col-md-6 col-xs-5">
             <h3 align="middle">Número de estudantes matriculados em abril de 2016 por regional</h3>
             <canvas id="myChart" width="auto" height="auto"></canvas>
@@ -87,11 +88,12 @@
             </script>
         </div>
         <!-- end of chart -->
+    </div>
     <!-- end of row -->
     <!-- new row -->
     <!-- Grafico referente ao
     	NÚMERO DE ESTUDANTES MATRICULADOS EM ABRIL DE 2016 POR GRAU ACADÊMICO NA REGIONAL CATALÃO -->
-    
+    <div class='row'>
         <div class="grafico col-md-6">
             <h3 align="middle">Número de estudantes matriculados em abril de 2016 por grau acadêmico na regional Catalão</h3>
             <canvas id="myChart3" width="auto" height="auto"></canvas>
@@ -129,6 +131,7 @@
             </script>
         </div>
         <!-- end of chart -->
+    </div>
 </div>
 
 <?php
