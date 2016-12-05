d7b19c11b51d9179ff57a01b0fbe4f95d1d8c55d
michaelsampietro <mssampietro@gmail.com>
2016-12-05 13:01:40 -0200

    arquivos finais

---
+++ includes/header.php
@@ -0,0 +1,29 @@
+<?php header('Content-Type: text/html; charset=utf-8'); ?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Bootstrap Example</title>
+	  	<meta charset="utf-8">
+	  	<meta name="viewport" content="width=device-width, initial-scale=1">
+	  	<!-- Arquivo CSS -->
+	  	<link rel="stylesheet" type="text/css" href="style.css">
+
+	  	<!-- Arquivo de gráficos -->
+	  	<script src="charts/Chart.js"></script>
+
+	  	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+  		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	</head>
+
+	<body>
+		<nav class="navbar navbar-default navbar-collapse">
+		  <div class="container-fluid">
+		    <div class="navbar-header">
+		      <a class="navbar-brand" href="../prograd_michael/index.php">
+		        <img alt="Brand" src="images/ufg_logo.png">
+		      </a>
+		      <a id="relatorios" class="navbar-brand" >Página de relatórios UFG por ano</a>
+		    </div>
+		  </div>
+		</nav>
